b14b2a607e778663cccea6cec85a7a3c123f0ab3
dvgiang <duongvangianghhbg152@gmail.com>
2024-10-30 20:09:53 +0700

    test

---
+++ server.js
@@ -0,0 +1,23 @@
+const https = require('https');
+const fs = require('fs');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// SSL options
+const sslOptions = {
+  key: fs.readFileSync('server.key'),
+  cert: fs.readFileSync('server.cert')
+};
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'homepage.html'));
+});
+
+const PORT = 3000;
+https.createServer(sslOptions, app).listen(PORT, () => {
+  console.log(`HTTPS server running at https://127.0.0.1:${PORT}`);
+});
